b1e0676def80750378074d8e98ae92e74499c1f2
nkey@stu.jsu.edu <nkey@stu.jsu.edu>
2020-03-03 15:59:22 -0600

    Added basic feature 1 information

--- src/edu/jsu/mcis/tas_SP20/Punch.java
+++ src/edu/jsu/mcis/tas_SP20/Punch.java
@@ -4,6 +4,22 @@ package edu.jsu.mcis.tas_SP20;
 
 public class Punch {
      
-    
+    //constructor 
+    Punch(Badge badge, int terminalid, int punchtypeid);
      
+    //other properties: ID, adjusted timestamp should be initialized to null or zero
+    
+    //timestamps - long, times - LocalTime
+    
+    //use this for setter/getter
+    printOriginalTimestamp(){
+        
+    }
+            
+    /* punch outputs: 
+    CLOCKED IN
+    CLOCKED OUT
+    TIME OUT
+    */       
+    
 }
